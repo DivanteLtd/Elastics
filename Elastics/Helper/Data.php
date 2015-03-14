3415178297f9013a7086ca5810411528a2f7ce81
Jakub Cegiełka <jcegielka>
2015-03-14 12:41:18 +0100

    Rename config scope to elastics; add index name setting

--- Elastics/Helper/Data.php
+++ Elastics/Helper/Data.php
@@ -10,6 +10,7 @@ use Elasticsearch\Client as Client;
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
     const ELASTICSEARCH_URI_CONFIG_PATH = "elastics/configuration/elasticsearch_server_uri";
+    const ELASTICSEARCH_INDEX_NAME_CONFIG_PATH = "elastics/configuration/elasticsearch_index_name";
 
     /**
      * @param null $store
@@ -31,5 +32,19 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
         $client = new Client($params);
 
         return $client;
+    }
+
+    /**
+     * @param null $store
+     *
+     * @return string
+     */
+    public function getElasticSearchIndexName($store = null){
+        $indexName = $this->scopeConfig->getValue(
+            self::ELASTICSEARCH_INDEX_NAME_CONFIG_PATH,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE, $store
+        );
+
+        return $indexName;
     }
 }
